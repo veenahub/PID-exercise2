25b00cacbdcf0904ae92aba815aff06f020e74a1
veenahub <veena.pattigulla@gmail.com>
2020-04-02 20:24:57 +0200

    adding pojofile

---
+++ java/Memberpojo.java
@@ -0,0 +1,32 @@
+package exercise2;
+
+public class Memberpojo {
+	private String id;
+	private String name;
+	private Boolean attendence;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Boolean getAttendence() {
+		return attendence;
+	}
+
+	public void setAttendence(Boolean attendence) {
+		this.attendence = attendence;
+	}
+
+}
